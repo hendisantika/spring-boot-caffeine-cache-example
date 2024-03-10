e92e0c14008e24e4b0513b212007f4143c95e79a
hendisantika <hendisantika@yahoo.co.id>
2024-03-10 16:50:14 +0700

    Add deleteById in  StudentService ✏️📗 :octocat: 🐳⬆

--- src/main/java/id/my/hendisantika/springbootcaffeinecacheexample/service/StudentService.java
+++ src/main/java/id/my/hendisantika/springbootcaffeinecacheexample/service/StudentService.java
@@ -4,6 +4,7 @@ import id.my.hendisantika.springbootcaffeinecacheexample.entity.Student;
 import id.my.hendisantika.springbootcaffeinecacheexample.exception.RecordNotFoundException;
 import id.my.hendisantika.springbootcaffeinecacheexample.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +30,15 @@ public class StudentService {
     public Student getById(Long id) {
         return studentRepository.findById(id)
                 .orElseThrow(() -> new RecordNotFoundException("Record not wound with id: " + id));
+    }
+
+    @CacheEvict(value = "StudentCache", key = "#id")
+    public void deleteById(Long id) {
+        //TODO: REMOVE IT. Its for Demo Only.
+        if (id == 1) {
+            // Simulate a failure scenario where student id is 1 and cannot be deleted.
+            throw new RuntimeException("Student cannot be deleted. It has magic ID");
+        }
+        studentRepository.deleteById(id);
     }
 }
