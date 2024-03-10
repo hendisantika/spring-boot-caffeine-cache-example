b317ad42fa238d0952b1d93219e02a25e89450cf
hendisantika <hendisantika@yahoo.co.id>
2024-03-10 16:46:25 +0700

    Add CacheConfig ✏️📗 :octocat: 🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootcaffeinecacheexample/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootcaffeinecacheexample.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-caffeine-cache-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/10/24
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+@EnableCaching
+@Configuration
+@EnableTransactionManagement
+public class CacheConfig {
+}
