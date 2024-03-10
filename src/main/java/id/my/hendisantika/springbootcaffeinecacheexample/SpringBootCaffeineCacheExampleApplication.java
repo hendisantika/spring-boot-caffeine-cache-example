a9cd28016635b9e9dd09f287886424174ebc9015
hendisantika <hendisantika@yahoo.co.id>
2024-03-10 16:44:28 +0700

    Create starter project ✏️📗 :octocat: 🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootcaffeinecacheexample/SpringBootCaffeineCacheExampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootcaffeinecacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaffeineCacheExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCaffeineCacheExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootcaffeinecacheexample/SpringBootCaffeineCacheExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootcaffeinecacheexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaffeineCacheExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
